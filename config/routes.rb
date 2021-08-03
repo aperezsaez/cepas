b687100fa5c06c4dfb7ce32023afdd0bad1558b8
nestorurda <nestorurda@gmail.com>
2021-08-02 22:00:41 -0400

    fix1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :magazines
   resources :oenologists
-  devise_for :users,  :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users
   resources :strains
   resources :straings
   resources :wines
